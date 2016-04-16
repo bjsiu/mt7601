958cf3b3d19f116b31297dfaf5792fa33e104a11
bjsiu <bjsiu0617@gmail.com>
2016-04-16 12:05:55 +0800

    Update driver.sh

--- driver.sh
+++ driver.sh
@@ -5,5 +5,5 @@ sudo make install
 sudo mkdir -p /etc/Wireless/RT2870STA/
 sudo cp RT2870STA.dat /etc/Wireless/RT2870STA/
 sudo modprobe mt7601Usta
-cp mcu/bin/MT7601.bin /lib/firmware/mt7601u.bin
+sudo cp mcu/bin/MT7601.bin /lib/firmware/mt7601u.bin
 exit
